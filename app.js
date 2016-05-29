3ac85abb6fc1e06f8946da19905af2b336ec62cc
Tonci <teodor.nikolov@komfo.com>
2016-05-30 00:10:50 +0300

    Initial version.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
+var express = require("express"),
+    path = require("path"),
+    bodyParser = require("body-parser"),
+    MongoClient = require('mongodb').MongoClient;
+
+var DBurl = 'mongodb://localhost:27017/IOTServer';
 
 var app = express();
+
+app.TH_COLLECTION = "th";
+
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,10 +16,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-var routes = require("./routes/routes.js")(app);
+MongoClient.connect(DBurl, function (err, database) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-// Initialize the app.
-var server = app.listen(process.env.PORT || 8080, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+    // Save database object from the callback for reuse.
+    db = database;
+    console.log("Database connection ready");
+
+    var routes = require("./routes/routes.js")(app, db);
+
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
+    });
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,23 @@
-var appRouter = function(app) {
+var appRouter = function(app, db) {
 	app.get("/th", function(req, res) {
-	    res.send('nothing yet');
+	    db.collection(app.TH_COLLECTION).find({}).toArray(function(err, objects) {
+		if (err) {
+			handleError(res, err.message, "Failed to get data.");
+		} else {
+			res.status(200).json(objects);
+		}
+		});
 	});
 
 	app.post("/th", function(req, res) {
-        res.send(req.body);
+		var th = { temperature: req.body.temperature, humidity: req.body.humidity, time: new Date() };
+		db.collection(app.TH_COLLECTION).insertOne(th, function(err, doc) {
+			if (err) {
+				handleError(res, err.message, "Failed to insert in mongo.");
+			} else {
+				res.status(201).json(doc.ops[0]);
+			}
+		});
 	});
 }
  
